c8de0ff624aeffe9baf8a334f735c073ff7d96a2
Sapphire327 <nochnoy_briz@mail.ru>
2026-05-30 17:35:35 +0300

    changed db back to postgres

--- src/db.ts
+++ src/db.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config'
 
-import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3"
+import { PrismaPg } from "@prisma/adapter-pg"
 import { PrismaClient } from "./generated/prisma"
 
-const adapter = new PrismaBetterSqlite3({ url: process.env.DATABASE_URL });
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 export const prisma = new PrismaClient({ adapter });
